074630451830715dba878b1c4f0597682aca3e15
mzalaya <alayaelm@utc.fr>
2024-10-26 23:37:09 +0200

    add torch version

---
+++ src/torch/kernels.py
@@ -0,0 +1,124 @@
+# Author: Mokhtar Z. Alaya <alayaelm@utc.fr>
+# License:
+
+import numpy as np
+import torch
+import functorch
+
+from sklearn.base import BaseEstimator
+
+from src.torch.utils import(
+uniform, rectangle, triangle,
+epanechnikov, biweight, tricube,
+gaussian, silverman
+)
+
+
+VALID_KERNELS_LIST = [
+    "uniform",
+    "rectangle",
+    "triangle",
+    "epanechnikov",
+    "biweight",
+    "tricube",
+    "gaussian",
+    "silverman",
+]
+
+import platform
+if platform.system() == 'Darwin':
+    device = torch.device("mps")
+elif platform.system() == 'Linux':
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+
+def space_kernel(kernel, x, Xt, bandwidth):
+    """
+    Space kernel
+    :param kernel: function
+    :param x: single point in R^d
+    :param Xt: single data point in R^d at time t
+    :param bandwidth: float
+    :return:
+    """
+    x_Xt_scaled = (x - Xt) / bandwidth
+    vectorize_kernel = torch.func.vmap(kernel)
+    kernel_vec_val = vectorize_kernel(x_Xt_scaled)
+
+    return torch.prod(kernel_vec_val)
+
+def time_kernel(kernel, aT, tT, bandwidth):
+    """
+    Time kernel
+    :param kernel:
+    :param aT:
+    :param tT:
+    :param bandwidth:
+    :return:
+    """
+    atT_scaled = (tT - aT) / bandwidth
+    return kernel(atT_scaled)
+
+class Kernel(BaseEstimator):
+    def __init__(
+            self,
+            *,
+            T=100,
+            d=2,
+            bandwidth=1.0,
+            space_kernel="gaussian",
+            time_kernel="gaussian",
+            metric="euclidean",
+            VALID_KERNELS_DIC={
+                "uniform": uniform,
+                "rectangle": rectangle,
+                "triangle": triangle,
+                "epanechnikov": epanechnikov,
+                "biweight": biweight,
+                "tricube": tricube,
+                "gaussian": gaussian,
+                "silverman": silverman,
+            }
+    ):
+        self.T = T
+        self.d = d
+        self.bandwidth = bandwidth
+        self.space_kernel = space_kernel
+        self.time_kernel = time_kernel
+        self.metric = metric
+        self.VALID_KERNELS_DIC = VALID_KERNELS_DIC
+
+    def fit(self, X, t):
+        """
+        Fit the Kernel Density model on the data.
+        :param X: array-like of shape (n_samples, n_features)
+                List of n_features-dimensional data points.  Each row corresponds to a single data point.
+        :param y:
+        :return:
+        """
+
+        tT = t / self.T
+        x = X[t]
+        numerator = torch.empty(self.T, dtype=torch.float).to(device)
+        denominator = torch.empty(self.T, dtype=torch.float).to(device)
+
+        if self.space_kernel in VALID_KERNELS_LIST and self.time_kernel in VALID_KERNELS_LIST:
+            for a in range(self.T):
+                Xa = X[a]
+                aT = a / self.T
+                skernel_name = self.VALID_KERNELS_DIC[self.space_kernel]
+                tkernel_name = self.VALID_KERNELS_DIC[self.time_kernel]
+
+
+                space_val = space_kernel(skernel_name, x, Xa, self.bandwidth)
+                time_val = time_kernel(tkernel_name, aT, tT, self.bandwidth)
+
+                numerator[a] = space_val * time_val
+                denominator[a] = time_val * space_val
+        else:
+            raise ValueError("Kernel type not supported")
+            
+        weights_t = numerator / denominator.sum()
+
+        return weights_t
---
+++ src/torch/utils.py
@@ -0,0 +1,142 @@
+# Author: Mokhtar Z. Alaya <alayaelm@utc.fr>
+# License:
+
+import math
+import torch
+
+import platform
+if platform.system() == 'Darwin':
+    device = torch.device("mps")
+elif platform.system() == 'Linux':
+    device = torch.device("cuda")
+else:
+    device = torch.device("cpu")
+def uniform(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return 1.
+    else:
+        return 0.
+
+def rectangle(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return 1/2
+    else:
+        return 0.
+
+def triangle(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return (1 - torch.abs(z))
+    else:
+        return 0.
+
+def epanechnikov(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return 3/4 * (1 - z**2)
+    else:
+        return 0.
+def biweight(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return 15/16 * (1 - z**2)**2
+    else:
+        return 0.
+
+def tricube(z):
+    z = torch.tensor(z).to(device)
+    if torch.abs(z) <= 1.:
+        return (1 - torch.abs(z) ** 3)*3
+    else:
+        return 0.
+
+def gaussian(z):
+    # z : BatchedTensor(lvl=1, bdim=0, value= tensor([0.], device='mps:0')
+    # z = torch._C._functorch.get_unwrapped(z)
+    # print(type(z))
+    # print(z)
+    return 1./torch.sqrt(2 * torch.as_tensor(torch.pi)) * torch.exp(-z ** 2 / 2)
+
+def silverman(z):
+    z = torch.tensor(z).to(device)
+    return 1/2 * torch.exp(-torch.abs(z) / math.sqrt(2)) * torch.sin(torch.abs(z) / math.sqrt(2) + torch.pi / 4)
+
+
+class ECDFTorch(torch.nn.Module):
+    def __init__(self, x, weights=None, side='right'):
+        super(ECDFTorch, self).__init__()
+
+        if side.lower() not in ['right', 'left']:
+            msg = "side can take the values 'right' or 'left'"
+            raise ValueError(msg)
+        self.side = side
+
+        if len(x.shape) != 1:
+            msg = 'x must be 1-dimensional'
+            raise ValueError(msg)
+
+        nobs = len(x)
+        if weights is not None:
+            assert len(weights) == nobs
+            sweights = torch.sum(weights)
+            assert sweights > 0.
+            sorted = torch.argsort(x).int()
+            x = x[sorted]
+            y = torch.cumsum(weights[sorted], dim=0)
+            y = y / sweights
+            self.x = torch.cat((torch.tensor([-torch.inf], device=x.device), x))
+            self.y = torch.cat((torch.tensor([0], device=y.device), y))
+            self.n = self.x.shape[0]
+
+        else:
+            x = torch.sort(x)[0]
+            y = torch.linspace(1. / nobs, 1, nobs, device=x.device)
+            self.x = torch.cat((torch.tensor([-torch.inf], device=x.device), x))
+            self.y = torch.cat((torch.tensor([0], device=y.device), y))
+            self.n = self.x.shape[0]
+
+    def forward(self, time):
+        tind = torch.searchsorted(self.x, time, side=self.side) - 1
+        return self.y[tind].to(device)
+
+def torch_wasserstein_loss(tensor_a,tensor_b):
+    #Compute the first Wasserstein distance between two 1D distributions.
+    return(torch_cdf_loss(tensor_a,tensor_b,p=1))
+
+def torch_energy_loss(tensor_a,tensor_b):
+    # Compute the energy distance between two 1D distributions.
+    return((2**0.5)*torch_cdf_loss(tensor_a,tensor_b,p=2))
+
+def torch_cdf_loss(tensor_a, tensor_b, p=1):
+    # last-dimension is weight distribution
+    # p is the norm of the distance, p=1 --> First Wasserstein Distance
+    # to get a positive weight with our normalized distribution
+    # we recommend combining this loss with other difference-based losses like L1
+
+    # normalize distribution, add 1e-14 to divisor to avoid 0/0
+    tensor_a = tensor_a / (torch.sum(tensor_a, dim=-1, keepdim=True) + 1e-14)
+    tensor_b = tensor_b / (torch.sum(tensor_b, dim=-1, keepdim=True) + 1e-14)
+    # make cdf with cumsum
+    cdf_tensor_a = torch.cumsum(tensor_a, dim=-1)
+    cdf_tensor_b = torch.cumsum(tensor_b, dim=-1)
+
+    # choose different formulas for different norm situations
+    if p == 1:
+        cdf_distance = torch.sum(torch.abs((cdf_tensor_a - cdf_tensor_b)), dim=-1)
+    elif p == 2:
+        cdf_distance = torch.sqrt(torch.sum(torch.pow((cdf_tensor_a - cdf_tensor_b), 2), dim=-1))
+    else:
+        cdf_distance = torch.pow(torch.sum(torch.pow(torch.abs(cdf_tensor_a - cdf_tensor_b), p), dim=-1), 1 / p)
+
+    cdf_loss = cdf_distance.mean()
+    return cdf_loss
+
+def torch_validate_distibution(tensor_a,tensor_b):
+    # Zero sized dimension is not supported by torch, we suppose there is no empty inputs
+    # Weights should be non-negetive, and with a positive and finite sum
+    # We suppose all conditions will be corrected by network training
+    # We only check the match of the size here
+    if tensor_a.size() != tensor_b.size():
+        raise ValueError("Input weight tensors must be of the same size")
